c2eb1da2e0802581264a974a8d7d72f1d2fb9b15
Carla Beatriz <cal.beatriz@hotmail.com>
2022-03-16 23:11:27 -0300

    Upload inicial

---
+++ AprendendoJava/src/UsandoOperadores/AreaDaCircunferencia.java
@@ -0,0 +1,14 @@
+package UsandoOperadores;
+
+public class AreaDaCircunferencia {
+		
+	public static void main (String [] args ) {
+	
+		double pi = 3.14;
+		double raio = 3.0;
+		double formula = pi * (raio * raio);
+		
+		System.out.println ("O resultado é: " + formula);
+	}
+	
+}
